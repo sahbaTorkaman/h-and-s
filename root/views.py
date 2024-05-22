0451a6690630bb8132a4af5ae44597531a9b10e3
sahbaTorkaman <sahbatorkamon@gmail.com>
2024-05-22 19:40:28 +0330

    ok

--- root/views.py
+++ root/views.py
@@ -5,7 +5,15 @@ from models import Services,Skill,Team,Portfolio
 def index (request):
     context = {
         'services':Services.objects.filter(status=True),
-        
+            'skills' : Skill.objects.filter(status=True),
+        'teams' : Team.objects.filter(status=True),
+        'portfolios' : Portfolio.objects.filter(status=True),
+        'servicescount' : Services.objects.all().count(),
+        'skillscount' : Skill.objects.all().count(),
+        'teamscount' : Team.objects.all().count(),
+        'portfolioscount' : Portfolio.objects.all().count(),
     }
+    return render(request,'root/index.html',context=context)
+    
 
 # Create your views here.
